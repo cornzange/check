49c97dcc26c6844fbd0ca9e90574539cb424340c
cornzange <cornzange@gmail.com>
2024-06-04 15:48:56 +0300

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/check/",
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
