f053213b5304929c2329e085f5df63440d3bec6a
cornzange <cornzange@gmail.com>
2024-06-04 16:14:27 +0300

    fix buffer 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/check/",
-  plugins: [react(), nodePolyfills()]
+  plugins: [react(), nodePolyfills()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupNodePolyFill()
+      ]
+    }
+  }
 })
